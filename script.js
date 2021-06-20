e55b4f8a369bb54b9f20571a5c4f058e606f5f8d
Joshua <j.blengino@yahoo.com>
2021-06-20 12:18:01 -0700

    contact form created

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 
 //document.querySelector('body').appendChild(h2); 
+
+
+//JS for contact form
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        status.classList.add('success');
+        form.reset()
+      }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
